f6ea169308ae632ef968ff00449ad26388a0211d
TRANCUTU <tutaph08467@fpt.edu.vn>
2020-11-27 12:30:50 +0700

    tu

--- app/src/main/java/com/example/soc_macmini_15/musicplayer/Activity/MainActivity.java
+++ app/src/main/java/com/example/soc_macmini_15/musicplayer/Activity/MainActivity.java
@@ -44,36 +44,424 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements AllSongFragment.createDataParse, FavSongFragment.createDataParsed{
 
+    private Menu menu;
+
+    private ImageButton imgBtnPlayPause, imgbtnReplay, imgBtnPrev, imgBtnNext, imgBtnSetting;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private SeekBar seekbarController;
+    private DrawerLayout mDrawerLayout;
+    private TextView tvCurrentTime, tvTotalTime;
+    private FloatingActionButton refreshSongs;
 
 
+    private ArrayList<SongsList> songList;
+    private int currentPosition;
+    private String searchText = "";
+    private SongsList currSong;
+    private final int MY_PERMISSION_REQUEST = 100;
+
+    private boolean checkFlag = false, repeatFlag = false, playContinueFlag = false, favFlag = true, playlistFlag = false;
+    private int allSongLength;
+
+    MediaPlayer mediaPlayer;
+    Handler handler;
+    Runnable runnable;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+        setPagerLayout();
+
+    }
+
+    /**
+     * Initialising the views
+     */
+
+    private void init() {
+        imgBtnPrev = findViewById(R.id.img_btn_previous);
+        imgBtnNext = findViewById(R.id.img_btn_next);
+        imgbtnReplay = findViewById(R.id.img_btn_replay);
+        imgBtnSetting = findViewById(R.id.img_btn_setting);
+
+        tvCurrentTime = findViewById(R.id.tv_current_time);
+        tvTotalTime = findViewById(R.id.tv_total_time);
+        refreshSongs = findViewById(R.id.btn_refresh);
+        seekbarController = findViewById(R.id.seekbar_controller);
+        viewPager = findViewById(R.id.songs_viewpager);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        imgBtnPlayPause = findViewById(R.id.img_btn_play);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        handler = new Handler();
+        mediaPlayer = new MediaPlayer();
+
+        toolbar.setTitleTextColor(getResources().getColor(R.color.text_color));
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        assert actionBar != null;
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.menu_icon);
+
+        // navigation view
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                item.setChecked(true);
+                mDrawerLayout.closeDrawers();
+                switch (item.getItemId()) {
+                    case R.id.nav_about:
+                        about();
+                        break;
+                    case R.id.navlogout:
+                        logout();
+                        break;
+                }
+                return true;
+            }
+        });
+
+        refreshSongs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setPagerLayout();
+                Toast.makeText(getApplicationContext(),"refresh",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //next
+        imgBtnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (checkFlag) {
+                    if (currentPosition + 1 < songList.size()) {
+                        attachMusic(songList.get(currentPosition + 1).getTitle(), songList.get(currentPosition + 1).getPath());
+                        currentPosition += 1;
+                        Toast.makeText(getApplicationContext(), "Next..", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+            }
+        });
+        //prev
+        imgBtnPrev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (checkFlag) {
+                    if (mediaPlayer.getCurrentPosition() > 10) {
+                        if (currentPosition - 1 > -1) {
+                            attachMusic(songList.get(currentPosition - 1).getTitle(), songList.get(currentPosition - 1).getPath());
+                            currentPosition = currentPosition - 1;
+                            Toast.makeText(getApplicationContext(), "Prev..", Toast.LENGTH_SHORT).show();
+                        } else {
+                            attachMusic(songList.get(currentPosition).getTitle(), songList.get(currentPosition).getPath());
+                        }
+                    } else {
+                        attachMusic(songList.get(currentPosition).getTitle(), songList.get(currentPosition).getPath());
+                    }
+                }
+            }
+        });
+
+        //play
+        imgBtnPlayPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (checkFlag) {
+                    if (mediaPlayer.isPlaying()) {
+                        mediaPlayer.pause();
+                        imgBtnPlayPause.setImageResource(R.drawable.play_icon);
+                        Toast.makeText(getApplicationContext(), "Pause..", Toast.LENGTH_SHORT).show();
+                    } else if (!mediaPlayer.isPlaying()) {
+                        mediaPlayer.start();
+                        imgBtnPlayPause.setImageResource(R.drawable.pause_icon);
+                        Toast.makeText(getApplicationContext(), "Play..", Toast.LENGTH_SHORT).show();
+                        playCycle();
+                    }
+                }
+            }
+        });
+
+        // phát lại
+        imgbtnReplay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (repeatFlag) {
+                    Toast.makeText(getApplicationContext(), "phát lại bật..", Toast.LENGTH_SHORT).show();
+                    mediaPlayer.setLooping(false);
+                    repeatFlag = false;
+                } else {
+                    Toast.makeText(getApplicationContext(), "phát lại tắt..", Toast.LENGTH_SHORT).show();
+                    mediaPlayer.setLooping(true);
+                    repeatFlag = true;
+                }
+            }
+        });
+    // setting
+        imgBtnSetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!playContinueFlag) {
+                    playContinueFlag = true;
+                    Toast.makeText(getApplicationContext(), "Lặp lại on", Toast.LENGTH_SHORT).show();
+                } else {
+                    playContinueFlag = false;
+                    Toast.makeText(getApplicationContext(), "Lặp lại off", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
 
     }
 
+    private void setPagerLayout() {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager(), getContentResolver());
+        viewPager.setAdapter(adapter);
+
+        tabLayout = findViewById(R.id.tabs);
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+        tabLayout.setupWithViewPager(viewPager);
+
+    }
+
+    private void about() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(getString(R.string.about))
+                .setMessage(getString(R.string.about_text))
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+    private void logout(){
+        Intent intent = new Intent(MainActivity.this, NguoiDungActivity.class);
+        startActivity(intent);
+        Toast.makeText(this, "Đăng xuất", Toast.LENGTH_SHORT).show();
+    }
+
+    // search
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        this.menu = menu;
+        getMenuInflater().inflate(R.menu.action_bar_menu, menu);
+        SearchManager manager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.menu_search).getActionView();
+        searchView.setSearchableInfo(manager.getSearchableInfo(getComponentName()));
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                searchText = newText;
+                queryText();
+                setPagerLayout();
+                return true;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(Gravity.START);
+                return true;
+            case R.id.menu_search:
+                Toast.makeText(this, "Search", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.menu_favorites:
+                if (checkFlag)
+                    if (mediaPlayer != null) {
+                        if (favFlag) {
+                            Toast.makeText(this, "Added to Favorites", Toast.LENGTH_SHORT).show();
+                            item.setIcon(R.drawable.ic_favorite_filled);
+                        }
+                    }
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+
+    }
+
+
+
+
+
+    private void attachMusic(String name, String path) {
+        imgBtnPlayPause.setImageResource(R.drawable.play_icon);
+        setTitle(name);
+        menu.getItem(1).setIcon(R.drawable.favorite_icon);
+        favFlag = true;
+
+        try {
+            mediaPlayer.reset();
+            mediaPlayer.setDataSource(path);
+            mediaPlayer.prepare();
+            setControls();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                imgBtnPlayPause.setImageResource(R.drawable.play_icon);
+                if (playContinueFlag) {
+                    if (currentPosition + 1 < songList.size()) {
+                        attachMusic(songList.get(currentPosition + 1).getTitle(), songList.get(currentPosition + 1).getPath());
+                        currentPosition += 1;
+                    } else {
+                        Toast.makeText(MainActivity.this, "PlayList Ended", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+    }
+
+    private void setControls() {
+        seekbarController.setMax(mediaPlayer.getDuration());
+        mediaPlayer.start();
+//        playCycle();
+        checkFlag = true;
+        if (mediaPlayer.isPlaying()) {
+            imgBtnPlayPause.setImageResource(R.drawable.pause_icon);
+            tvTotalTime.setText(getTimeFormatted(mediaPlayer.getDuration()));
+        }
+
+        seekbarController.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser) {
+                    mediaPlayer.seekTo(progress);
+                    tvCurrentTime.setText(getTimeFormatted(progress));
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    /**
+     * Function to play the song using a thread
+     */
+    private void playCycle() {
+        try {
+            seekbarController.setProgress(mediaPlayer.getCurrentPosition());
+            tvCurrentTime.setText(getTimeFormatted(mediaPlayer.getCurrentPosition()));
+            if (mediaPlayer.isPlaying()) {
+                runnable = new Runnable() {
+                    @Override
+                    public void run() {
+                        playCycle();
+
+                    }
+                };
+                handler.postDelayed(runnable, 100);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // finalTimerString
+
+    private String getTimeFormatted(long milliseconds) {
+        String finalTimerString = "";
+        String secondsString = "";
+
+        // Convert total duration into time
+        int hours = (int)( milliseconds / (1000*60*60));
+        int minutes = (int)(milliseconds % (1000*60*60)) / (1000*60);
+        int seconds = (int) ((milliseconds % (1000*60*60)) % (1000*60) / 1000);
+        // Add hours if there
+        if(hours > 0){
+            finalTimerString = hours + ":";
+        }
+
+        // Prepending 0 to seconds if it is one digit
+        if(seconds < 10){
+            secondsString = "0" + seconds;
+        }else{
+            secondsString = "" + seconds;}
+
+        finalTimerString = finalTimerString + minutes + ":" + secondsString;
+
+        // return timer string
+        return finalTimerString;
+    }
+
+
     @Override
     public void onDataPass(String name, String path) {
+        Toast.makeText(this, name, Toast.LENGTH_LONG).show();
+        attachMusic(name, path);
+    }
 
+    @Override
+    public void getLength(int length) {
+        this.allSongLength = length;
     }
 
     @Override
     public void fullSongList(ArrayList<SongsList> songList, int position) {
-
-    }
-
-    @Override
-    public int getPosition() {
-        return 0;
+        this.songList = songList;
+        this.currentPosition = position;
+        this.playlistFlag = songList.size() == allSongLength;
+        this.playContinueFlag = !playlistFlag;
     }
 
     @Override
     public String queryText() {
-        return null;
+        return searchText.toLowerCase();
     }
 
+    @Override
+    public void currentSong(SongsList songsList) {
+        this.currSong = songsList;
+    }
+
+    @Override
+    public int getPosition() {
+        return currentPosition;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mediaPlayer.release();
+        handler.removeCallbacks(runnable);
+    }
 }
--- app/src/main/java/com/example/soc_macmini_15/musicplayer/Fragments/AllSongFragment.java
+++ app/src/main/java/com/example/soc_macmini_15/musicplayer/Fragments/AllSongFragment.java
@@ -31,7 +31,24 @@ import java.util.ArrayList;
 public class AllSongFragment extends ListFragment {
 
 
+    private static ContentResolver contentResolver1;
 
+    public ArrayList<SongsList> songsList;
+    public ArrayList<SongsList> newList;
+
+    private ListView listView;
+
+    private createDataParse createDataParse;
+    private ContentResolver contentResolver;
+
+    public static Fragment getInstance(int position, ContentResolver mcontentResolver) {
+        Bundle bundle = new Bundle();
+        bundle.putInt("pos", position);
+        AllSongFragment tabFragment = new AllSongFragment();
+        tabFragment.setArguments(bundle);
+        contentResolver1 = mcontentResolver;
+        return tabFragment;
+    }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -42,7 +59,7 @@ public class AllSongFragment extends ListFragment {
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-
+        createDataParse = (createDataParse) context;
     }
 
     @Override
@@ -52,10 +69,83 @@ public class AllSongFragment extends ListFragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        listView = view.findViewById(R.id.list_playlist);
+        contentResolver = contentResolver1;
+        setContent();
+    }
+
+
+    public void setContent() {
+        boolean searchedList = false;
+        songsList = new ArrayList<>();
+        newList = new ArrayList<>();
+        getMusic();
+        SongAdapter adapter = new SongAdapter(getContext(), songsList);
+        if (!createDataParse.queryText().equals("")) {
+            adapter = onQueryTextChange();
+            adapter.notifyDataSetChanged();
+            searchedList = true;
+        } else {
+            searchedList = false;
+        }
+        createDataParse.getLength(songsList.size());
+        listView.setAdapter(adapter);
+
+        //click vào danh sách
+        final boolean finalSearchedList = searchedList;
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Toast.makeText(getContext(), "You clicked :\n" + songsList.get(position), Toast.LENGTH_SHORT).show();
+                if (!finalSearchedList) {
+                    createDataParse.onDataPass(songsList.get(position).getTitle(), songsList.get(position).getPath());
+                    createDataParse.fullSongList(songsList, position);
+                } else {
+                    createDataParse.onDataPass(newList.get(position).getTitle(), newList.get(position).getPath());
+                    createDataParse.fullSongList(songsList, position);
+                }
+            }
+        });
+    }
+
+            // quyen danh sach nhac
+    public void getMusic() {
+        Uri songUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        Cursor songCursor = contentResolver.query(songUri, null, null, null, null);
+        if (songCursor != null && songCursor.moveToFirst()) {
+            int songTitle = songCursor.getColumnIndex(MediaStore.Audio.Media.TITLE);
+            int songArtist = songCursor.getColumnIndex(MediaStore.Audio.Media.ARTIST);
+            int songPath = songCursor.getColumnIndex(MediaStore.Audio.Media.DATA);
+
+            do {
+                songsList.add(new SongsList(songCursor.getString(songTitle), songCursor.getString(songArtist), songCursor.getString(songPath)));
+            } while (songCursor.moveToNext());
+            songCursor.close();
+        }
+    }
+
+    public SongAdapter onQueryTextChange() {
+        String text = createDataParse.queryText();
+        for (SongsList songs : songsList) {
+            String title = songs.getTitle().toLowerCase();
+            if (title.contains(text)) {
+                newList.add(songs);
+            }
+        }
+        return new SongAdapter(getContext(), newList);
 
     }
 
 
     public interface createDataParse {
+        public void onDataPass(String name, String path);
+
+        public void fullSongList(ArrayList<SongsList> songList, int position);
+
+        public String queryText();
+
+        public void currentSong(SongsList songsList);
+        public void getLength(int length);
     }
+
 }
